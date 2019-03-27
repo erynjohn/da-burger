ca66daf44e9122cf78488175692bb1099b41c69c
Eryn <johnmary1010@gmail.com>
2019-03-27 15:37:49 -0500

    update jawsdb

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ if(process.env.JAWSDB_URL) {
     var connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 
-    connection = mysql.createConnection({
+    var connection = mysql.createConnection({
         host: "localhost",
         port: 3306,
         user: process.env.USER,
