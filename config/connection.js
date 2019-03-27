57189adfd89d1c1224a7c0673ce51b3705e86838
Eryn <johnmary1010@gmail.com>
2019-03-27 15:46:36 -0500

    update connection

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,12 @@
+var connection;
 var mysql = require("mysql");
 require('dotenv').config()
-if(process.env.JAWSDB_URL) {
-    var connection = mysql.createConnection(process.env.JAWSDB_URL);
+if (process.env.JAWSDB_URL) {
+    // Database is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 
-    var connection = mysql.createConnection({
+    connection = mysql.createConnection({
         host: "localhost",
         port: 3306,
         user: process.env.USER,
